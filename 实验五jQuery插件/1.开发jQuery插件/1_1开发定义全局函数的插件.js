b5f6a72c42092832aa9b1d35c9cf6456781e0fe6
iluhao <luhao_6@foxmail.com>
2017-11-21 21:22:30 +0800

    开发jQuery插件
    开发jQuery插件

--- 实验五jQuery插件/1.开发jQuery插件/1_1开发定义全局函数的插件.js
+++ 实验五jQuery插件/1.开发jQuery插件/1_1开发定义全局函数的插件.js
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>开发定义全局函数的插件</title>
-	<script type="text/javascript" src="../jquery.js"></script>
-	<script type="text/javascript">
-		
-	</script>
-</head>
-<body>
-	
-</body>
-</html>
+(function( $ ){
+	$.fn.showhtml = function() {
+		alert(this.html());
+	}
+})( jQuery )
